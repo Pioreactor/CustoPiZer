fb56766e917d76dc3517adbeb5d4a94d1dc86bf0
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2022-11-27 14:08:41 -0500

    add username/pass to mosquitto

--- workspace/scripts/09-install-mosquitto.sh
+++ workspace/scripts/09-install-mosquitto.sh
@@ -14,10 +14,16 @@ if [ "$LEADER" == "1" ]; then
     sudo apt-get install -y mosquitto mosquitto-clients
     sudo systemctl enable mosquitto.service
 
+    # create auth file
+    echo "pioreactor:raspberry" > /etc/mosquitto/pw.txt
+    # hash password
+    mosquitto_passwd -U /etc/mosquitto/pw.txt
+
     grep -qxF 'autosave_interval 300'  /etc/mosquitto/mosquitto.conf || echo "autosave_interval 300" | sudo tee /etc/mosquitto/mosquitto.conf -a
     grep -qxF 'listener 1883'          /etc/mosquitto/mosquitto.conf || echo "listener 1883"         | sudo tee /etc/mosquitto/mosquitto.conf -a
     grep -qxF 'protocol mqtt'          /etc/mosquitto/mosquitto.conf || echo "protocol mqtt"         | sudo tee /etc/mosquitto/mosquitto.conf -a
     grep -qxF 'listener 9001'          /etc/mosquitto/mosquitto.conf || echo "listener 9001"         | sudo tee /etc/mosquitto/mosquitto.conf -a
     grep -qxF 'protocol websockets'    /etc/mosquitto/mosquitto.conf || echo "protocol websockets"   | sudo tee /etc/mosquitto/mosquitto.conf -a
-    grep -qxF 'allow_anonymous true'   /etc/mosquitto/mosquitto.conf || echo "allow_anonymous true"   | sudo tee /etc/mosquitto/mosquitto.conf -a
+    grep -qxF 'allow_anonymous false'  /etc/mosquitto/mosquitto.conf || echo "allow_anonymous false" | sudo tee /etc/mosquitto/mosquitto.conf -a
+    grep -qxF 'password_file /etc/mosquitto/pw.txt'  /etc/mosquitto/mosquitto.conf || echo "password_file /etc/mosquitto/pw.txt" | sudo tee /etc/mosquitto/mosquitto.conf -a
 fi
