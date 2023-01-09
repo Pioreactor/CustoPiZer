031a2a12eb3527a95dec8aa45bf671ce4e8fb9fa
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-01-08 20:47:58 -0500

    run services after firstboot, adding new logging config

--- workspace/scripts/01-make-pioreactor-user.sh
+++ workspace/scripts/01-make-pioreactor-user.sh
@@ -19,3 +19,20 @@ usermod -a -G gpio $USERNAME
 usermod -a -G spi $USERNAME
 usermod -a -G i2c $USERNAME
 usermod -a -G www-data $USERNAME
+# Note that the following also occurs in firstboot.sh that is created by the RPi imager:
+# We should move this into our image eventually...
+
+#   if [ "$FIRSTUSER" != "pioreactor" ]; then
+#      usermod -l "pioreactor" "$FIRSTUSER"
+#      usermod -m -d "/home/pioreactor" "pioreactor"
+#      groupmod -n "pioreactor" "$FIRSTUSER"
+#      if grep -q "^autologin-user=" /etc/lightdm/lightdm.conf ; then
+#         sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/autologin-user=pioreactor/"
+#      fi
+#      if [ -f /etc/systemd/system/getty@tty1.service.d/autologin.conf ]; then
+#         sed /etc/systemd/system/getty@tty1.service.d/autologin.conf -i -e "s/$FIRSTUSER/pioreactor/"
+#      fi
+#      if [ -f /etc/sudoers.d/010_pi-nopasswd ]; then
+#         sed -i "s/^$FIRSTUSER /pioreactor /" /etc/sudoers.d/010_pi-nopasswd
+#      fi
+#   fi
