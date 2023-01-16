825961dba85e17ef0800c115de7852cba13dad74
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-01-16 14:45:57 -0500

    this was mistakenly deleted

--- workspace/scripts/02-install-python-and-pip.sh
+++ workspace/scripts/02-install-python-and-pip.sh
@@ -12,5 +12,6 @@ install_cleanup_trap
 
 apt-get install -y python3-pip
 apt-get install -y python3-dev # needed to build CLoader in pyyaml
+pip3 install pip -U # update to latest
 pip3 config set global.disable-pip-version-check true # don't check for latest pip
 pip3 config set global.root-user-action "ignore"
