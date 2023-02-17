1356ad572382fa37a5d1798d57e9077ee6c11bf8
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-02-17 16:14:22 -0500

    small updates, won't make it into 23.2.17, that's okay

--- workspace/scripts/10-install-db.sh
+++ workspace/scripts/10-install-db.sh
@@ -10,6 +10,7 @@ source /common.sh
 install_cleanup_trap
 
 USERNAME=pioreactor
+STORAGE=/home/$USERNAME/.pioreactor/storage
 
 # install sqlite3 on all machines, as I expect I'll use it on workers one day.
 sudo apt-get install -y sqlite3
@@ -18,10 +19,16 @@ sudo apt-get install -y sqlite3
 
 if [ "$LEADER" == "1" ]; then
 
-    DB_LOC=/home/$USERNAME/.pioreactor/storage/pioreactor.sqlite
+    DB_LOC=$STORAGE/pioreactor.sqlite
+
 
     sudo -u $USERNAME touch $DB_LOC
-    chmod 666 $DB_LOC
+    sudo -u $USERNAME touch $DB_LOC-shm
+    sudo -u $USERNAME touch $DB_LOC-wal
+    sudo chmod 666 $DB_LOC
+    sudo chmod 666 $DB_LOC-shm
+    sudo chmod 666 $DB_LOC-wal
+    sudo chmod 777 $STORAGE
     sqlite3 $DB_LOC < /files/sql/sqlite_configuration.sql
     sqlite3 $DB_LOC < /files/sql/create_tables.sql
     sqlite3 $DB_LOC < /files/sql/create_triggers.sql
--- workspace/scripts/files/bash/update_ui.sh
+++ workspace/scripts/files/bash/update_ui.sh
@@ -4,34 +4,35 @@ set -e
 
 export LC_ALL=C
 
-SOURCE=$1
+SRC_TAR=$1
 TAG=$2
 
 UI_FOLDER=/var/www/pioreactorui
+SRC_FOLDER=/tmp/pioreactorui-"$TAG"
 
 
 # unpack source provided
-tar -xvzf "$SOURCE" -C /tmp
+tar -xvzf "$SRC_TAR" -C /tmp
 
 # copy data over
 # use rsync because we want to merge custom yamls the user has, we any updates to our own yamls.
-rsync -ap --ignore-existing $UI_FOLDER/contrib/ /tmp/pioreactorui-"$TAG"/contrib/ 2>/dev/null || :
-cp -p $UI_FOLDER/huey.db      /tmp/pioreactorui-"$TAG" 2>/dev/null || :
-cp -p $UI_FOLDER/huey.db-shm  /tmp/pioreactorui-"$TAG" 2>/dev/null || :
-cp -p $UI_FOLDER/huey.db-wal  /tmp/pioreactorui-"$TAG" 2>/dev/null || :
-cp -p $UI_FOLDER/.env         /tmp/pioreactorui-"$TAG" 2>/dev/null || :
+rsync -ap --ignore-existing $UI_FOLDER/contrib/ "$SRC_FOLDER"/contrib/ 2>/dev/null || :
+cp -p $UI_FOLDER/huey.db      "$SRC_FOLDER" 2>/dev/null || :
+cp -p $UI_FOLDER/huey.db-shm  "$SRC_FOLDER" 2>/dev/null || :
+cp -p $UI_FOLDER/huey.db-wal  "$SRC_FOLDER" 2>/dev/null || :
+cp -p $UI_FOLDER/.env         "$SRC_FOLDER" 2>/dev/null || :
 
 # swap folders
 sudo rm -rf $UI_FOLDER
 mkdir $UI_FOLDER
-cp -rp /tmp/pioreactorui-"$TAG"/. $UI_FOLDER
+cp -rp "$SRC_FOLDER"/. $UI_FOLDER
 sudo chgrp -R www-data $UI_FOLDER
 
 # install any new requirements
 sudo pip install -r $UI_FOLDER/requirements.txt
 
 # cleanup
-rm -rf /tmp/pioreactorui-"$TAG"
+rm -rf "$SRC_FOLDER"
 
 # restart services
 sudo systemctl restart lighttpd.service
