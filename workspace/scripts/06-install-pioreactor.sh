589279e9188d2ac5f0e8054ded778898269250c9
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-02-23 15:03:37 -0500

    fix location of contrib in .pioreactor'

--- workspace/scripts/06-install-pioreactor.sh
+++ workspace/scripts/06-install-pioreactor.sh
@@ -15,9 +15,9 @@ PIO_DIR=/home/$USERNAME/.pioreactor
 sudo -u $USERNAME mkdir -p $PIO_DIR
 sudo -u $USERNAME mkdir -p $PIO_DIR/storage
 sudo -u $USERNAME mkdir -p $PIO_DIR/plugins
-sudo -u $USERNAME mkdir -p $PIO_DIR/plugins/ui/jobs
-sudo -u $USERNAME mkdir -p $PIO_DIR/plugins/ui/automations/{dosing,led,temperature}
-sudo -u $USERNAME mkdir -p $PIO_DIR/plugins/ui/charts
+sudo -u $USERNAME mkdir -p $PIO_DIR/plugins/ui/contrib/jobs
+sudo -u $USERNAME mkdir -p $PIO_DIR/plugins/ui/contrib/automations/{dosing,led,temperature}
+sudo -u $USERNAME mkdir -p $PIO_DIR/plugins/ui/contrib/charts
 echo "Directory for adding Python code, see docs: https://docs.pioreactor.com/developer-guide/intro-plugins" > $PIO_DIR/plugins/README.txt
 echo "Directory for adding to the UI using yaml files, see docs: https://docs.pioreactor.com/developer-guide/adding-plugins-to-ui" > $PIO_DIR/plugins/ui/README.txt
 
@@ -30,7 +30,6 @@ if [ "$LEADER" == "1" ]; then
     sudo -u $USERNAME cp /files/config.example.ini $PIO_DIR/config.ini
     sudo pip3 install --ignore-installed "pioreactor[leader] @ https://github.com/Pioreactor/pioreactor/releases/download/$PIO_VERSION/pioreactor-$PIO_VERSION-py3-none-any.whl"
 fi
-
 
 
 if [ "$WORKER" == "1" ]; then
