bd29514abb6d8f4dd6280c83955754771ae1f4f6
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-02-25 22:20:13 -0500

    adding build info

--- workspace/scripts/99-finish.sh
+++ workspace/scripts/99-finish.sh
@@ -12,4 +12,8 @@ install_cleanup_trap
 
 sudo apt-get clean
 
+
+touch /home/pioreactor/.pioreactor/.image_info
+echo -e "CUSTOPIZER_GIT_COMMIT=$CUSTOPIZER_GIT_COMMIT"  >> /home/pioreactor/.pioreactor/.image_info
+
 echo_green "Complete!"
