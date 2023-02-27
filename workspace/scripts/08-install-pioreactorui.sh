2221be46dd94923bd41eae9bc10e7c113ecf81f1
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-02-27 12:30:04 -0500

    the owner was root of /var/www, but it should be pioreactor

--- workspace/scripts/08-install-pioreactorui.sh
+++ workspace/scripts/08-install-pioreactorui.sh
@@ -53,6 +53,7 @@ if [ "$LEADER" == "1" ]; then
 
     # make correct permissions in new www folders and files
     # https://superuser.com/questions/19318/how-can-i-give-write-access-of-a-folder-to-all-users-in-linux
+    chown -R pioreactor /var/www
     chgrp -R www-data /var/www
     chmod -R g+w /var/www
     find /var/www -type d -exec chmod 2775 {} \;
