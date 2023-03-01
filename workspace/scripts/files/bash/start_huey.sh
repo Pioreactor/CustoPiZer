0ce44844dd39867bb47e2bab10f504586c31fdbc
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-02-28 19:45:38 -0500

    better

--- workspace/scripts/files/bash/start_huey.sh
+++ workspace/scripts/files/bash/start_huey.sh
@@ -5,11 +5,11 @@ set -e
 
 export LC_ALL=C
 
-mkdir /tmp/pioreactorui_cache 
+mkdir -p /tmp/pioreactorui_cache
 touch /tmp/pioreactorui_cache/cache.db
 touch /tmp/pioreactorui_cache/cache.db-shm
 touch /tmp/pioreactorui_cache/cache.db-wal
 chmod -R 770 /tmp/pioreactorui_cache/
 chown -R pioreactor:www-data /tmp/pioreactorui_cache
 
-(cd /var/www/pioreactorui/; huey_consumer tasks.huey -s 60 -n -b 1.0 -w 2 -f -C)
+(cd /var/www/pioreactorui/; huey_consumer tasks.huey -s 60 -n -b 1.0 -w 2 -f -C &)
