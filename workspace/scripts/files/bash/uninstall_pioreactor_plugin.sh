09ff029fd65f76edfc4e1634f97b98a2838e8be7
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-03-19 21:32:08 -0400

    conditional on leader

--- workspace/scripts/files/bash/uninstall_pioreactor_plugin.sh
+++ workspace/scripts/files/bash/uninstall_pioreactor_plugin.sh
@@ -8,19 +8,21 @@ export LC_ALL=C
 plugin_name=$1
 
 
-# the below can fail, and will fail on a worker
+leader_hostname=$(crudini --get /home/pioreactor/.pioreactor/config.ini cluster.topology leader_hostname)
 
-# delete yamls from pioreactorui
-install_folder=$(python3 -c "import site; print(site.getsitepackages()[0])")/${plugin_name//-/_}
-(cd "$install_folder"/ui/contrib/ && find ./ -type f) | awk '{print "/var/www/pioreactorui/contrib/"$1}' | xargs rm
+if [ "$leader_hostname" == "$(hostname)" ]; then
+    # delete yamls from pioreactorui
+    install_folder=$(python3 -c "import site; print(site.getsitepackages()[0])")/${plugin_name//-/_}
+    (cd "$install_folder"/ui/contrib/ && find ./ -type f) | awk '{print "/var/www/pioreactorui/contrib/"$1}' | xargs rm
 
-# TODO: remove sections from config.ini
-# this is complicated because sometimes we edit sections, instead of adding full sections. Ex: we edit [PWM] in relay plugin.
+    # TODO: remove sections from config.ini
+    # this is complicated because sometimes we edit sections, instead of adding full sections. Ex: we edit [PWM] in relay plugin.
 
-
-sudo pip3 uninstall  -y "$plugin_name"
-
-# broadcast to cluster
-pios sync-configs
+    sudo pip3 uninstall  -y "$plugin_name"
+    # broadcast to cluster
+    pios sync-configs
+else
+    sudo pip3 uninstall  -y "$plugin_name"
+fi
 
 exit 0
