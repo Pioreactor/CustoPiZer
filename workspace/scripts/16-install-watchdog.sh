f057de93f6e5337ba0ca36e0c14658b0fa447083
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-10-13 15:07:11 -0400

    adding watchdog

---
+++ workspace/scripts/16-install-watchdog.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export LC_ALL=C
+
+
+source /common.sh
+install_cleanup_trap
+
+
+echo 'dtparam=watchdog=on' >> /boot/config.txt
+sudo apt-get install watchdog
+echo 'watchdog-device = /dev/watchdog' >> /etc/watchdog.conf
+echo 'watchdog-timeout = 90' >> /etc/watchdog.conf
+echo 'max-load-1 = 24' >> /etc/watchdog.conf
+echo 'interface = wlan0' >> /etc/watchdog.conf
+
+
+sudo systemctl enable watchdog
+sudo systemctl start watchdog
+sudo systemctl status watchdog
