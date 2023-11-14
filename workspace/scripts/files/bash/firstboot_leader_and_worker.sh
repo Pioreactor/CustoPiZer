4db469e9a4daf3a309d476f7d56123359c2f8664
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2023-11-13 20:33:52 -0500

    fix dependencies again; speed up firstboot.sh

--- workspace/scripts/files/bash/firstboot_leader_and_worker.sh
+++ workspace/scripts/files/bash/firstboot_leader_and_worker.sh
@@ -9,6 +9,7 @@ USERNAME=pioreactor
 PIO_DIR=/home/$USERNAME/.pioreactor
 SSH_DIR=/home/$USERNAME/.ssh
 DB_LOC=$(crudini --get $PIO_DIR/config.ini storage database)
+HOSTNAME=$(hostname)
 
 # clean up if this needs to run again.
 sudo -u $USERNAME rm -f $SSH_DIR/{authorized_keys,known_hosts,id_rsa,id_rsa.pub}
@@ -18,15 +19,15 @@ sudo -u $USERNAME touch $SSH_DIR/known_hosts
 
 sudo -u $USERNAME ssh-keygen -q -t rsa -N '' -f $SSH_DIR/id_rsa
 sudo -u $USERNAME cat $SSH_DIR/id_rsa.pub > $SSH_DIR/authorized_keys
-sudo -u $USERNAME ssh-keyscan "$(hostname)".local >> $SSH_DIR/known_hosts
+sudo -u $USERNAME ssh-keyscan "$HOSTNAME".local >> $SSH_DIR/known_hosts
 
-crudini --set --ini-options=nospace $PIO_DIR/config.ini cluster.topology leader_hostname "$(hostname)"
-crudini --set --ini-options=nospace $PIO_DIR/config.ini cluster.topology leader_address "$(hostname)".local
+crudini --ini-options=nospace --set  $PIO_DIR/config.ini cluster.topology leader_hostname "$HOSTNAME" \
+                              --set  $PIO_DIR/config.ini cluster.topology leader_address "$HOSTNAME".local \
+                              --set  $PIO_DIR/config.ini cluster.inventory "$HOSTNAME" 1
 
 sqlite3 $DB_LOC "INSERT OR IGNORE INTO experiments (created_at, experiment, description) VALUES (STRFTIME('%Y-%m-%dT%H:%M:%f000Z', 'NOW'), 'Demo experiment', 'This is a demo experiment. Feel free to click around. When you are ready, click the [New experiment] above.');"
 
 
-sudo -u $USERNAME touch $PIO_DIR/config_"$(hostname)".ini # set with the correct read/write permissions
-printf '# Any settings here are specific to %s, and override the settings in config.ini\n\n' "$(hostname)" >> $PIO_DIR/config_"$(hostname)".ini
-cp "$PIO_DIR/config_$(hostname).ini" "$PIO_DIR/unit_config.ini"
-crudini --set --ini-options=nospace "$PIO_DIR/config.ini" cluster.inventory "$(hostname)" 1
+sudo -u $USERNAME touch $PIO_DIR/config_"$HOSTNAME".ini # set with the correct read/write permissions
+printf '# Any settings here are specific to %s, and override the settings in config.ini\n\n' "$HOSTNAME" >> $PIO_DIR/config_"$HOSTNAME".ini
+cp "$PIO_DIR/config_$HOSTNAME.ini" "$PIO_DIR/unit_config.ini"
