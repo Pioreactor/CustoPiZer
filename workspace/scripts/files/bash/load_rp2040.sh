bf806b1a852bf1119ec371f0dd7b19c649a6119c
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2024-01-08 13:58:08 -0500

    replace openocd with openocd v0.12.0, and add new interfaces for loading to rp2040

--- workspace/scripts/files/bash/load_rp2040.sh
+++ workspace/scripts/files/bash/load_rp2040.sh
@@ -5,4 +5,15 @@ set -e
 
 export LC_ALL=C
 
-openocd -f interface/raspberrypi-swd.cfg -f target/rp2040.cfg -c "init" -c "reset halt" -c "load_image /usr/local/bin/main.elf" -c "resume 0x20000000" -c "exit" || true
+
+# Check if the Raspberry Pi version is 5
+RPI_MODEL=$(grep -o "Raspberry Pi 5" /proc/cpuinfo)
+
+# Choose the correct interface based on Raspberry Pi version
+if [ "$RPI_MODEL" = "Raspberry Pi 5" ]; then
+    INTERFACE=interface/raspberrypi-linuxgpiod-chip4.cfg
+else
+    INTERFACE=interface/raspberrypi-linuxgpiod-chip0.cfg
+fi
+
+openocd -f $INTERFACE -f target/rp2040.cfg -c "init" -c "reset halt" -c "load_image /usr/local/bin/main.elf" -c "resume 0x20000000" -c "exit" || true
