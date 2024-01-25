cf556d3ad45b905219e728318560749360317d14
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2024-01-25 15:56:36 -0500

    config for entering 64 bit images

--- enter_image.sh
+++ enter_image.sh
@@ -2,4 +2,7 @@
 
 : ${1?"Usage: $0 IMAGE_NAME"}
 
-docker run -it --rm --privileged -v $(pwd)/workspace/$1:/$1 ghcr.io/octoprint/custopizer:latest /CustoPiZer/enter_image /$1
+
+CONFIG=$2
+
+docker run -it --rm --privileged -v $(pwd)/workspace/$1:/$1  -v $CONFIG:/CustoPiZer/config.local ghcr.io/octoprint/custopizer:latest /CustoPiZer/enter_image /$1
