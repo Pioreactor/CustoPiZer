c5d3865cec4f165ca5524389797126d6e8f6f493
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2024-04-03 12:50:02 -0400

    can't reload

--- workspace/scripts/16-add-default-network-connections.sh
+++ workspace/scripts/16-add-default-network-connections.sh
@@ -15,5 +15,3 @@ cp /files/system/NetworkManager/PioreactorLocalLink.nmconnection /etc/NetworkMan
 # 600 is required for security reasons, and nm won't register them if not 600
 sudo chmod 600 /etc/NetworkManager/system-connections/PioreactorAP.nmconnection
 sudo chmod 600 /etc/NetworkManager/system-connections/PioreactorLocalLink.nmconnection
-
-sudo nmcli connection reload
