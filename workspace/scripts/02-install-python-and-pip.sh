a693186d225dff17f87f5e66b724763e0b80acf1
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2024-07-18 11:33:14 -0400

    fix pip

--- workspace/scripts/02-install-python-and-pip.sh
+++ workspace/scripts/02-install-python-and-pip.sh
@@ -13,7 +13,7 @@ install_cleanup_trap
 apt-get install -y python3-pip
 apt-get install -y python3-dev # needed to build CLoader in pyyaml
 sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED || true # see jeff gerlings blog post
-sudo pip3 install pip==0.24
+sudo pip3 install pip==24.0
 sudo pip3 install wheel==0.41.2
 
 # these live in /root/.config/pip/pip.conf
