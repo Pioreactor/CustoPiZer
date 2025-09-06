0be14a30671ce58aaa2bf33926fe36c8db158e18
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2025-09-05 21:28:56 -0400

    python3.13 now

--- workspace/scripts/03-configure-rpi.sh
+++ workspace/scripts/03-configure-rpi.sh
@@ -73,15 +73,9 @@ if [ "$WORKER" == "1" ]; then
 
 fi
 
-# the below will remove swap, which should help extend the life of SD cards:
-# https://raspberrypi.stackexchange.com/questions/169/how-can-i-extend-the-life-of-my-sd-card
-sudo apt-get remove dphys-swapfile -y
-
-# remove some redundant programs
-sudo apt-get remove system-config-printer -y
 
 # remove depend. of uninstalled programs.
-sudo apt-get autoremove -y
+# sudo apt-get autoremove -y
 
 # put /tmp into memory, as we write to it a lot.
 echo "tmpfs /tmp tmpfs defaults,noatime 0 0" | sudo tee -a /etc/fstab
