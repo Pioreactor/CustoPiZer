b4c0afc422078a6f3228d2c5313e56646d9fa90f
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2025-09-05 20:55:38 -0400

    python3.13 now

--- workspace/scripts/02-install-python-and-pip.sh
+++ workspace/scripts/02-install-python-and-pip.sh
@@ -12,7 +12,7 @@ install_cleanup_trap
 
 apt-get install -y python3-pip
 apt-get install -y python3-dev # needed to build CLoader in pyyaml
-sudo rm -rf /usr/lib/python3.11/EXTERNALLY-MANAGED || true # see jeff gerlings blog post
+sudo rm -rf /usr/lib/python3.13/EXTERNALLY-MANAGED || true # see jeff gerlings blog post
 sudo pip3 install pip==24.0
 sudo pip3 install wheel==0.41.2
 
