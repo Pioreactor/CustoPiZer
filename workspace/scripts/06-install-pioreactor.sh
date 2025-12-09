ba385712ff2bbf032ae950ffe9101b57abb1ac67
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2025-12-08 22:11:16 -0500

    old

--- workspace/scripts/06-install-pioreactor.sh
+++ workspace/scripts/06-install-pioreactor.sh
@@ -169,13 +169,11 @@ if [ "$LEADER" == "1" ]; then
 
     if [ "$PIO_VERSION" == "develop" ]; then
         sudo -u pioreactor "$PIO_VENV/bin/pip" install \
-          --find-links "$TMP_WHEELS" \
           "pioreactor[leader_worker] @ git+https://github.com/pioreactor/pioreactor.git@develop#egg=pioreactor&subdirectory=core" \
           --index-url https://piwheels.org/simple \
           --extra-index-url https://pypi.org/simple
     else
       sudo -u pioreactor "$PIO_VENV/bin/pip" install \
-        --find-links "$TMP_WHEELS" \
         "pioreactor[leader] @ https://github.com/Pioreactor/pioreactor/releases/download/$PIO_VERSION/pioreactor-$PIO_VERSION-py3-none-any.whl" \
         --index-url https://piwheels.org/simple \
         --extra-index-url https://pypi.org/simple
