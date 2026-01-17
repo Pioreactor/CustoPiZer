be601425936f1852eff465a355ea9a30c2c31b66
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-01-16 22:05:30 -0500

    don't fail if .local isn't found

--- workspace/scripts/99-finish.sh
+++ workspace/scripts/99-finish.sh
@@ -13,19 +13,19 @@ install_cleanup_trap
 sudo apt-get clean
 
 USERNAME=pioreactor
-PIO_DIR=/home/$USERNAME/.pioreactor
+DOT_PIOREACTOR=/home/$USERNAME/.pioreactor
 
 ensure_dot_pioreactor_tree_group_is_www_data() {
-    if [ -d "$PIO_DIR" ]; then
-        find "$PIO_DIR" -mindepth 0 \( ! -user "$USERNAME" -o ! -group www-data \) -exec chown -h "$USERNAME":www-data {} +
-        chmod g+w "$PIO_DIR"
-        find "$PIO_DIR" -mindepth 1 \( -type d -o -type f \) -exec chmod g+w {} +
-        find "$PIO_DIR" -type d ! -perm -2000 -exec chmod g+s {} +
+    if [ -d "$DOT_PIOREACTOR" ]; then
+        find "$DOT_PIOREACTOR" -mindepth 0 \( ! -user "$USERNAME" -o ! -group www-data \) -exec chown -h "$USERNAME":www-data {} +
+        chmod g+w "$DOT_PIOREACTOR"
+        find "$DOT_PIOREACTOR" -mindepth 1 \( -type d -o -type f \) -exec chmod g+w {} +
+        find "$DOT_PIOREACTOR" -type d ! -perm -2000 -exec chmod g+s {} +
     fi
 }
 
-sudo -u $USERNAME touch $PIO_DIR/.image_info
-echo -e "CUSTOPIZER_GIT_COMMIT=$CUSTOPIZER_GIT_COMMIT" | sudo -u $USERNAME tee -a $PIO_DIR/.image_info > /dev/null
+sudo -u $USERNAME touch $DOT_PIOREACTOR/.image_info
+echo -e "CUSTOPIZER_GIT_COMMIT=$CUSTOPIZER_GIT_COMMIT" | sudo -u $USERNAME tee -a $DOT_PIOREACTOR/.image_info > /dev/null
 
 ensure_dot_pioreactor_tree_group_is_www_data
 
--- workspace/scripts/files/bash/firstboot_leader.sh
+++ workspace/scripts/files/bash/firstboot_leader.sh
@@ -7,17 +7,17 @@ export LC_ALL=C
 
 PIO_VENV=/opt/pioreactor/venv
 USERNAME=pioreactor
-PIO_DIR=/home/$USERNAME/.pioreactor
+DOT_PIOREACTOR=/home/$USERNAME/.pioreactor
 SSH_DIR=/home/$USERNAME/.ssh
-DB_LOC=$("$PIO_VENV/bin/crudini" --get $PIO_DIR/config.ini storage database)
+DB_LOC=$("$PIO_VENV/bin/crudini" --get $DOT_PIOREACTOR/config.ini storage database)
 HOSTNAME=$(hostname)
 
 ensure_dot_pioreactor_tree_group_is_www_data() {
-    if [ -d "$PIO_DIR" ]; then
-        find "$PIO_DIR" -mindepth 0 \( ! -user "$USERNAME" -o ! -group www-data \) -exec chown -h "$USERNAME":www-data {} +
-        chmod g+w "$PIO_DIR"
-        find "$PIO_DIR" -mindepth 1 \( -type d -o -type f \) -exec chmod g+w {} +
-        find "$PIO_DIR" -type d ! -perm -2000 -exec chmod g+s {} +
+    if [ -d "$DOT_PIOREACTOR" ]; then
+        find "$DOT_PIOREACTOR" -mindepth 0 \( ! -user "$USERNAME" -o ! -group www-data \) -exec chown -h "$USERNAME":www-data {} +
+        chmod g+w "$DOT_PIOREACTOR"
+        find "$DOT_PIOREACTOR" -mindepth 1 \( -type d -o -type f \) -exec chmod g+w {} +
+        find "$DOT_PIOREACTOR" -type d ! -perm -2000 -exec chmod g+s {} +
     fi
 }
 # clean up if this needs to run again.
@@ -28,22 +28,23 @@ sudo -u $USERNAME touch $SSH_DIR/known_hosts
 
 sudo -u $USERNAME ssh-keygen -q -t rsa -N '' -f $SSH_DIR/id_rsa
 sudo -u $USERNAME cat $SSH_DIR/id_rsa.pub > $SSH_DIR/authorized_keys
-sudo -u $USERNAME ssh-keyscan "$HOSTNAME".local >> $SSH_DIR/known_hosts
+sudo -u $USERNAME ssh-keyscan "$HOSTNAME".local >> $SSH_DIR/known_hosts 2>/dev/null || true
+sudo -u $USERNAME ssh-keyscan "$HOSTNAME" >> $SSH_DIR/known_hosts 2>/dev/null || true
 
-sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set $PIO_DIR/config.ini cluster.topology leader_hostname "$HOSTNAME"
-sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set $PIO_DIR/config.ini cluster.topology leader_address "$HOSTNAME".local
-sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set $PIO_DIR/config.ini mqtt broker_address "$HOSTNAME".local
+sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set $DOT_PIOREACTOR/config.ini cluster.topology leader_hostname "$HOSTNAME"
+sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set $DOT_PIOREACTOR/config.ini cluster.topology leader_address "$HOSTNAME".local
+sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set $DOT_PIOREACTOR/config.ini mqtt broker_address "$HOSTNAME".local
 
 sqlite3 $DB_LOC "INSERT OR IGNORE INTO experiments (created_at, experiment, description) VALUES (STRFTIME('%Y-%m-%dT%H:%M:%fZ', 'NOW'), 'Demo experiment', 'This is a demo experiment. Feel free to click around.  When you are ready, create a new experiment in the dropdown to the left.');"
 
 
 # create leader's config file (still can use one even if not a worker.)
-sudo -u $USERNAME touch "$PIO_DIR/config_$HOSTNAME.ini" # set with the correct read/write permissions
-printf '# Any settings here are specific to %s, the leader, and override the settings in config.ini\n\n' "$HOSTNAME" >> "$PIO_DIR/config_$HOSTNAME.ini"
+sudo -u $USERNAME touch "$DOT_PIOREACTOR/config_$HOSTNAME.ini" # set with the correct read/write permissions
+printf '# Any settings here are specific to %s, the leader, and override the settings in config.ini\n\n' "$HOSTNAME" >> "$DOT_PIOREACTOR/config_$HOSTNAME.ini"
 
-sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set "$PIO_DIR/config_$HOSTNAME.ini" cluster.topology leader_address 127.0.0.1
-sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set "$PIO_DIR/config_$HOSTNAME.ini" mqtt broker_address 127.0.0.1
+sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set "$DOT_PIOREACTOR/config_$HOSTNAME.ini" cluster.topology leader_address 127.0.0.1
+sudo -u $USERNAME "$PIO_VENV/bin/crudini" --ini-options=nospace --set "$DOT_PIOREACTOR/config_$HOSTNAME.ini" mqtt broker_address 127.0.0.1
 
-cp -a "$PIO_DIR/config_$HOSTNAME.ini" "$PIO_DIR/unit_config.ini"
+cp -a "$DOT_PIOREACTOR/config_$HOSTNAME.ini" "$DOT_PIOREACTOR/unit_config.ini"
 
 ensure_dot_pioreactor_tree_group_is_www_data
