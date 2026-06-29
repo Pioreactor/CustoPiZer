a25de4faf318b740ced8cde8691823cdc400d88a
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-06-28 21:33:07 -0400

    fix for wifi placement and use crudnini

--- workspace/scripts/files/bash/bootfs_wifi.sh
+++ workspace/scripts/files/bash/bootfs_wifi.sh
@@ -4,33 +4,16 @@ set -e
 
 export LC_ALL=C
 
+VENV_BIN="${PIO_VENV:-/opt/pioreactor/venv}/bin"
+CRUDINI="$VENV_BIN/crudini"
 WIFI_CONFIG=/boot/firmware/wifi.ini
 
 if [ ! -f "$WIFI_CONFIG" ]; then
     exit 0
 fi
 
-SSID=$(awk -F= '
-    /^\[wifi\]$/ { in_wifi=1; next }
-    /^\[/ { in_wifi=0 }
-    in_wifi && $1 ~ /^[[:space:]]*ssid[[:space:]]*$/ {
-        sub(/^[[:space:]]*/, "", $2)
-        sub(/[[:space:]]*$/, "", $2)
-        print $2
-        exit
-    }
-' "$WIFI_CONFIG")
-
-PASSPHRASE=$(awk -F= '
-    /^\[wifi\]$/ { in_wifi=1; next }
-    /^\[/ { in_wifi=0 }
-    in_wifi && $1 ~ /^[[:space:]]*passphrase[[:space:]]*$/ {
-        sub(/^[[:space:]]*/, "", $2)
-        sub(/[[:space:]]*$/, "", $2)
-        print $2
-        exit
-    }
-' "$WIFI_CONFIG")
+SSID=$("$CRUDINI" --get "$WIFI_CONFIG" wifi ssid || :)
+PASSPHRASE=$("$CRUDINI" --get "$WIFI_CONFIG" wifi passphrase || :)
 
 if [ -z "$SSID" ] || [ -z "$PASSPHRASE" ]; then
     exit 1
