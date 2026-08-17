68d2ca4477154f9e268c0f43d30d5f72c4f1e899
CamDavidsonPilon <cam.davidson.pilon@gmail.com>
2026-08-17 15:02:59 -0400

    huey

--- workspace/scripts/files/bash/start_pioreactor_huey.sh
+++ workspace/scripts/files/bash/start_pioreactor_huey.sh
@@ -7,9 +7,9 @@ image_profile="${PIOREACTOR_IMAGE_PROFILE:-standard}"
 
 case "$image_profile" in
   zero_w_worker)
-    exec "$huey_consumer" pioreactor.web.tasks.huey -n -b 1.0 -w 1 -f -C -d 0.25
+    exec "$huey_consumer" pioreactor.web.tasks.huey -b 1.0 -w 1 -f -C -d 0.25
     ;;
   *)
-    exec "$huey_consumer" pioreactor.web.tasks.huey -n -b 1.0 -w 10 -f -C -d 0.01
+    exec "$huey_consumer" pioreactor.web.tasks.huey -b 1.0 -w 10 -f -C -d 0.01
     ;;
 esac
